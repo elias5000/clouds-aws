2d53b75781756c9f461b421d610423f24091fbdd
Frank Wittig <frank.wittig@here.com>
2018-02-19 13:49:34 +0100

    Prevent continuation if virtualenv creation fails

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 # little helper to setup local development environment
 
 if [ ! -d virtualenv ]; then
-  virtualenv -p python3 virtualenv
+  virtualenv -p python3 virtualenv || exit 1
 fi
 
 source ./virtualenv/bin/activate
