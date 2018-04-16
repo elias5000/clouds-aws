8a46c3e2c2acd6d53be359d34d8ed2eccc04ecdc
Frank Wittig <frank.wittig@here.com>
2018-04-16 17:33:24 +0200

    Add capability for stack creation

--- src/clouds_aws/remote_stack/aws_client.py
+++ src/clouds_aws/remote_stack/aws_client.py
@@ -9,7 +9,7 @@ from botocore.exceptions import ClientError
 from clouds_aws.local_stack.helpers import dump_json
 
 LOG = logging.getLogger(__name__)
-CAPABILITIES = ['CAPABILITY_IAM']
+CAPABILITIES = ['CAPABILITY_IAM', 'CAPABILITY_NAMED_IAM']
 
 
 class CloudFormationError(Exception):
