elias5000/clouds-aws

commit c716192e30f8b4fc718270b84b403b848dded0e2

Catch ClientErrors on higher level
This mainly reduces redundant code that does little more than log and
exit. Specific cases are caught locally where they happen.

src/clouds_aws/cli/delete.py
@@ -2,8 +2,6 @@
 
 import logging
 
-from botocore.exceptions import ClientError
-
 from clouds_aws.cli.events import poll_events
 from clouds_aws.remote_stack import RemoteStack, list_stacks as remote_stacks
 
@@ -41,7 +39,6 @@ def cmd_delete(args):
         LOG.warning("You have to apply force to delete %s", args.stack)
         exit(1)
 
-    try:
         remote_stack = RemoteStack(args.stack, args.region, args.profile)
         remote_stack.load()
         remote_stack.delete()
@@ -49,7 +46,3 @@ def cmd_delete(args):
         # poll until stable state is reached
         if args.events or args.wait:
             poll_events(remote_stack, args.events)
-
-    except ClientError as err:
-        LOG.error(err)
-        exit(1)

src/clouds_aws/__init__.py
@@ -2,9 +2,8 @@
 
 import argparse
 import logging
-import sys
 
-import botocore.exceptions
+from botocore.exceptions import ClientError
 
 from .cli import add_parsers
 
@@ -42,4 +41,8 @@ def main():
         LOG.setLevel(logging.DEBUG)
         logging.basicConfig(level=logging.DEBUG)
 
-    args.func(args)
+    try:
+        args.func(args)
+    except ClientError as err:
+        LOG.error(err)
+        exit(1)

src/clouds_aws/cli/describe.py
@@ -5,7 +5,7 @@ import logging
 from tabulate import tabulate
 
 from clouds_aws.local_stack.helpers import dump_json
-from clouds_aws.remote_stack import RemoteStack, CloudFormationError
+from clouds_aws.remote_stack import RemoteStack
 
 LOG = logging.getLogger(__name__)
 
@@ -29,33 +29,28 @@ def cmd_describe(args):
     :param args:
     :return:
     """
-    try:
-        stack = RemoteStack(args.stack, args.region, args.profile)
-        stack.load()
+    stack = RemoteStack(args.stack, args.region, args.profile)
+    stack.load()
 
-        if args.json:
-            print(dump_json({
-                "Parameters": stack.parameters,
-                "Outputs": stack.outputs,
-                "Resources": stack.resources
-            }))
-            return
+    if args.json:
+        print(dump_json({
+            "Parameters": stack.parameters,
+            "Outputs": stack.outputs,
+            "Resources": stack.resources
+        }))
+        return
 
-        if stack.parameters:
-            print(tabulate(sorted(stack.parameters.items()), ("Parameter", "Value")))
-            print()
-
-        if stack.outputs:
-            print(tabulate(sorted(stack.outputs.items()), ("Output", "Value")))
-            print()
-
-        print(tabulate(
-            sorted([(key, val["ResourceType"], val["PhysicalResourceId"])
-                    for key, val in stack.resources.items()]),
-            ("Resource", "Type", "PhysicalId")
-        ))
+    if stack.parameters:
+        print(tabulate(sorted(stack.parameters.items()), ("Parameter", "Value")))
         print()
 
-    except CloudFormationError as err:
-        LOG.error(err)
-        exit(1)
+    if stack.outputs:
+        print(tabulate(sorted(stack.outputs.items()), ("Output", "Value")))
+        print()
+
+    print(tabulate(
+        sorted([(key, val["ResourceType"], val["PhysicalResourceId"])
+                for key, val in stack.resources.items()]),
+        ("Resource", "Type", "PhysicalId")
+    ))
+    print()

src/clouds_aws/cli/dump.py
@@ -4,7 +4,7 @@ import logging
 
 from clouds_aws.local_stack import LocalStack
 from clouds_aws.remote_stack import RemoteStack
-from clouds_aws.remote_stack.aws_client import CloudFormation, CloudFormationError
+from clouds_aws.remote_stack.aws_client import CloudFormation
 
 LOG = logging.getLogger(__name__)
 
@@ -47,14 +47,9 @@ def dump_stack(region, profile, stack, force):
     :param force: force overwrite
     :return:
     """
-    try:
-        LOG.info("Loading remote stack %s", stack)
-        remote = RemoteStack(stack, region, profile)
-        remote.load()
-
-    except CloudFormationError as err:
-        LOG.error(err)
-        return
+    LOG.info("Loading remote stack %s", stack)
+    remote = RemoteStack(stack, region, profile)
+    remote.load()
 
     LOG.info("Creating local stack %s", stack)
     local = LocalStack(stack)

src/clouds_aws/cli/list.py
@@ -1,9 +1,13 @@
 """ list command parser definition """
 
+import logging
+
 from tabulate import tabulate
 
-from ..local_stack import list_stacks as local_stacks
-from ..remote_stack import list_stacks as remote_stacks
+from clouds_aws.local_stack import list_stacks as local_stacks
+from clouds_aws.remote_stack import list_stacks as remote_stacks
+
+LOG = logging.getLogger(__name__)
 
 
 def add_parser(subparsers):

src/clouds_aws/cli/update.py
@@ -38,34 +38,31 @@ def cmd_update(args):
     local_stack = load_local_stack(args.stack)
     remote_stack = RemoteStack(args.stack, args.region, args.profile)
 
-    if args.stack in remote_stacks(args.region, args.profile):
-        remote_stack.load()
-        try:
+    try:
+        if args.stack in remote_stacks(args.region, args.profile):
+            remote_stack.load()
             remote_stack.update(
                 local_stack.template,
                 local_stack.parameters
             )
-        except ClientError as err:
-            if "No updates are to be performed" in str(err):
-                LOG.warning("No updates are to be performed")
-                exit(0)
-            else:
-                LOG.error(err)
-                exit(1)
 
-    elif args.create_missing:
-        try:
+        elif args.create_missing:
             remote_stack.create(
                 local_stack.template,
                 local_stack.parameters
             )
-        except ClientError as err:
-            LOG.error(err)
+
+        else:
+            LOG.error("Stack %s does not exist. Not updating without explicit create", args.stack)
             exit(1)
 
-    else:
-        LOG.error("Stack %s does not exist. Not updating without explicit create", args.stack)
-        exit(1)
+    except ClientError as err:
+        if "No updates are to be performed" in str(err):
+            LOG.warning("No updates are to be performed")
+            exit(0)
+
+        # throw up if not "no updates" case
+        raise err
 
     # poll until stable state is reached
     if args.events or args.wait:

src/clouds_aws/cli/validate.py
@@ -2,8 +2,8 @@
 
 import logging
 
-from clouds_aws.local_stack import list_stacks as local_stacks
 from clouds_aws.cli.common import load_local_stack
+from clouds_aws.local_stack import list_stacks as local_stacks
 from clouds_aws.remote_stack.aws_client import CloudFormation, CloudFormationError
 
 LOG = logging.getLogger(__name__)

commit 61ae47142f94dc3986a8a77b83b25817ec1dfe04

Fix broken indentation
Fixes a regression introduced by
c716192 Catch ClientErrors on higher level

setup.py
@@ -11,7 +11,7 @@ except KeyError:
 setup(
     name='clouds-aws',
 
-    version='0.3.7-3%s' % SNAPSHOT,
+    version='0.3.7-4%s' % SNAPSHOT,
 
     description='A tool for easy handling of AWS CloudFormation stacks as code.',
     long_description="""Clouds-aws a CLI tool easy handling if CloudFormation stacks as code.

src/clouds_aws/cli/delete.py
@@ -39,10 +39,10 @@ def cmd_delete(args):
         LOG.warning("You have to apply force to delete %s", args.stack)
         exit(1)
 
-        remote_stack = RemoteStack(args.stack, args.region, args.profile)
-        remote_stack.load()
-        remote_stack.delete()
+    remote_stack = RemoteStack(args.stack, args.region, args.profile)
+    remote_stack.load()
+    remote_stack.delete()
 
-        # poll until stable state is reached
-        if args.events or args.wait:
-            poll_events(remote_stack, args.events)
+    # poll until stable state is reached
+    if args.events or args.wait:
+        poll_events(remote_stack, args.events)